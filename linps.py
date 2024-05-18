93680d81152af9450b558621a59b047bcfee4dbf
Grace Marie Gibbins <gibbins@cpu37.elgato.hpc.arizona.edu>
2024-05-17 19:15:38 -0700

    Seperated lhc from linps

--- linps.py
+++ linps.py
@@ -16,6 +16,7 @@ from pyDOE import lhs
 import time
 
 from gpsclass import CalcGalaxyPowerSpec
+from lhc import create_lhs_samples
 
 #Galaxy Bias Parameter
 bias = np.array([1.9,-0.6,(-4./7)*(1.9-1),(32./315.)*(1.9-1)])
@@ -26,15 +27,6 @@ prior = np.array([[20,100], #H0
                   [0.1,.3], #omega_c
                   [1.2e-9,2.7e-9], #As
                   [0.8,1.2]]) #ns
-
-#Number of cosmo params in prior array
-n_dim = 5
-
-#g: inputs number of wanted samples and prior array to output number of samples with randomly set cosmology from prior
-def create_lhs_samples(n_samples , prior):
-    lhs_samples = lhs(n_dim, n_samples) #creates lhc with values 0 to 1
-    cosmo_samples = prior[:,0] + (prior[:,1] - prior[:,0]) * lhs_samples #scales each value to the given priors
-    return cosmo_samples
 
 #Creates linear power spectra from priors - input into galaxy ps class
 def get_linps(params):
